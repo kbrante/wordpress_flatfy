4d17ea22538f126b93da3b05a38026a78e6d59e3
karina BRANTE <karina.brante@gmail.com>
2017-08-29 14:50:19 +0200

    add menu

--- header.php
+++ header.php
@@ -30,27 +30,6 @@
             </div>
         </div>
 
-        <div class="row maNavbar">
-            <nav class="navbar navbar-default">
-                <div class="container-fluid">
-                    <div class="navbar-header">
-                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                            <span class="sr-only">Toggle navigation</span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
-                        <a class="navbar-brand animated shake" href="#home">Flatfy</a>
-                    </div>
-                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                        <ul class="nav navbar-nav navbar-right">
-                            <li><a href="#whatis">What is ?</a></li>
-                            <li class="dropdown"><a href="#useit">Use It</a></li>
-                            <li class="dropdown"><a href="#screen">Screenshot</a></li>
-                            <li class="dropdown"><a href="#credits">Crédits</a></li>
-                            <li class="dropdown"><a href="#contact">Contact</a></li>
-                        </ul>
-                    </div><!-- /.navbar-collapse -->
-                </div><!-- /.container-fluid -->
-            </nav>
-        </div>
+        <?php
+        wp_nav_menu( array( 'theme_location' => 'header-menu' ) );
+         ?>
