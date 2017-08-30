24a0994339ac0be781fdb15a86c4b7d09ef8a4b6
karina BRANTE <karina.brante@gmail.com>
2017-08-30 11:53:59 +0200

    add svg authorised

--- functions.php
+++ functions.php
@@ -1,4 +1,13 @@
 <?php function register_my_menu() {
   register_nav_menu('header-menu',__( 'Header Menu' ));
 }
-add_action( 'init', 'register_my_menu' ); ?>
+add_action( 'init', 'register_my_menu' );
+
+/* Autoriser les fichiers SVG */
+function wpc_mime_types($mimes) {
+  $mimes['svg'] = 'image/svg+xml';
+  return $mimes;
+}
+add_filter('upload_mimes', 'wpc_mime_types');
+
+add_theme_support('post-thumbnails');
